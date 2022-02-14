24a8cbf5e9dbe6a23c130e7d68f0f66be8c10cae
Bruna Brazao <brunaa@gmail.com>
2022-02-14 20:55:30 +0000

    Add `carousel_props` to HomepagePresenter
    Rubocop messed up a bit by touching the other method on the presenter
    but this comment is simply introducing new props.
    
    Content still up for grabs

--- app/presenters/homepage_presenter.rb
+++ app/presenters/homepage_presenter.rb
@@ -6,4 +6,17 @@ class HomepagePresenter < BasePresenter
       OpenStruct.new(menu_item: 'Book a free demo', link: root_url)
     ]
   end
+
+  def carousel_props
+    [
+      OpenStruct.new(carousel_item_class: 'active first-carousel-item',
+                     header: 'We exist to facilitate conversations',
+                     copy: 'Engage employees, empower and train managers by helping them to become the best version of themselves.',
+                     cta: 'Learn more'),
+      OpenStruct.new(carousel_item_class: 'second-carousel-item',
+                     header: 'Your partner for continuous performance management',
+                     copy: 'Effectively manage expectations through open communication',
+                     cta: 'Book a demo')
+    ]
+  end
 end
