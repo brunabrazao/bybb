af9752db5ee22b7abb268842f3cce79c3f68c73c
Bruna Brazao <brunaa@gmail.com>
2022-02-16 00:07:07 +0000

    Update main navigation link for How it works page
    And update spec

--- app/presenters/homepage_presenter.rb
+++ app/presenters/homepage_presenter.rb
@@ -2,7 +2,7 @@ class HomepagePresenter < BasePresenter
   def main_navigation_links
     [
       OpenStruct.new(menu_item: 'Home', link: root_url),
-      OpenStruct.new(menu_item: 'How it works', link: root_url),
+      OpenStruct.new(menu_item: 'How it works', link: how_it_works_path),
       OpenStruct.new(menu_item: 'Book a free demo', link: root_url)
     ]
   end
--- spec/presenters/homepage_presenter_spec.rb
+++ spec/presenters/homepage_presenter_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe HomepagePresenter do
     it 'returns an array with menu items' do
       expect(subject.main_navigation_links).to eq(
         [OpenStruct.new(menu_item: 'Home', link: Rails.application.routes.url_helpers.root_url),
-         OpenStruct.new(menu_item: 'How it works', link: Rails.application.routes.url_helpers.root_url), OpenStruct.new(menu_item: 'Book a free demo', link: Rails.application.routes.url_helpers.root_url)]
+         OpenStruct.new(menu_item: 'How it works', link: Rails.application.routes.url_helpers.how_it_works_path),
+         OpenStruct.new(menu_item: 'Book a free demo', link: Rails.application.routes.url_helpers.root_url)]
       )
     end
   end
