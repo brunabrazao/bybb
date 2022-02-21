26c56af185f2a85fc14994e39ba6932de5e59645
Bruna Brazao <brunaa@gmail.com>
2022-02-21 23:51:35 +0000

    Add relationship for user and report models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reports
 end
