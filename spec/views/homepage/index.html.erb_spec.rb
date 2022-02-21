e58cb920aa8a1664b8f435a47da751ef3cf99dfe
Bruna Brazao <brunaa@gmail.com>
2022-02-21 23:02:33 +0000

    Update homepage index spec
    Although there's not much logic here, I wanted to ensure we're rendering
    the partials correctly.

--- spec/views/homepage/index.html.erb_spec.rb
+++ spec/views/homepage/index.html.erb_spec.rb
@@ -1,8 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'homepage/index' do
-  it 'renders a boiler plate content' do
+  let(:featurette_props) do
+    double(:featurette_props,
+           featurette_item_class: 'class',
+           header: 'header',
+           sub_header: 'sub-header',
+           copy: 'copy',
+           img: '')
+  end
+
+  let(:presenter) do
+    instance_double('HomepagePresenter', :presenter,
+                    carousel_props: [],
+                    featurette_props: [
+                      featurette_props
+                    ])
+  end
+
+  before do
+    allow(HomepagePresenter).to receive(:new).and_return(presenter)
+    allow(presenter).to receive(:carousel_props).and_return([])
+    allow(presenter).to receive(:featurette_props).and_return([featurette_props])
+    assign(:presenter, presenter)
+
+    stub_template 'views/homepage/_carousel.html.erb' => 'carousel'
+    stub_template 'views/homepage/_featurette.html.erb' => 'featurette'
+  end
+
+  it 'renders carousel partial' do
     render
-    expect(rendered).to have_content('Boilerplate')
+    expect(response).to render_template(partial: 'carousel', locals: { presenter: presenter })
+  end
+
+  it 'renders featurette partial' do
+    render
+    expect(response).to render_template(partial: 'featurette',
+                                        locals: { featurette_item_class: 'class', header: 'header',
+                                                  sub_header: 'sub-header', copy: 'copy', img: '' })
   end
 end
