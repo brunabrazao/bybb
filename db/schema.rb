50d4214910881559aa8bf0861845c18ef5af7fec
Bruna Brazao <brunaa@gmail.com>
2022-02-22 23:43:41 +0000

    Add role id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_021956) do
+ActiveRecord::Schema.define(version: 2022_02_22_230139) do
 
   create_table "organisations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -53,12 +53,14 @@ ActiveRecord::Schema.define(version: 2022_02_22_021956) do
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
     t.integer "organisation_id"
+    t.integer "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
