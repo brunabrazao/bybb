7f7ae5b482a334cb85887c905538c49fb4915c58
Bruna Brazao <brunaa@gmail.com>
2022-03-19 13:00:24 +0000

    Add Feed model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_225515) do
+ActiveRecord::Schema.define(version: 20_220_319_125_252) do
+  create_table 'feeds', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
+    t.string 'organisation_title'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
 
   create_table "organisations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
