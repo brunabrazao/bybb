81963aa2460f8e1a33125080d090f0ed94f4310f
Bruna Brazao <brunaa@gmail.com>
2022-03-23 20:29:23 +0000

    Update review cycle form to hide organisation_id
    And set organisation id upon review cycle creation

--- app/controllers/reviews_cycles_controller.rb
+++ app/controllers/reviews_cycles_controller.rb
@@ -1,31 +1,25 @@
 class ReviewsCyclesController < ApplicationController
-  before_action :set_reviews_cycle, only: %i[ show edit update destroy ]
+  before_action :set_reviews_cycle, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
-  # GET /reviews_cycles or /reviews_cycles.json
   def index
     @reviews_cycles = ReviewsCycle.all
   end
 
-  # GET /reviews_cycles/1 or /reviews_cycles/1.json
-  def show
-  end
+  def show; end
 
-  # GET /reviews_cycles/new
   def new
-    @reviews_cycle = ReviewsCycle.new
+    @reviews_cycle = current_user.organisation.reviews_cycles.build
   end
 
-  # GET /reviews_cycles/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /reviews_cycles or /reviews_cycles.json
   def create
-    @reviews_cycle = ReviewsCycle.new(reviews_cycle_params)
+    @reviews_cycle = current_user.organisation.reviews_cycles.build(reviews_cycle_params)
 
     respond_to do |format|
       if @reviews_cycle.save
-        format.html { redirect_to reviews_cycle_url(@reviews_cycle), notice: "Reviews cycle was successfully created." }
+        format.html { redirect_to reviews_cycle_url(@reviews_cycle), notice: 'Reviews cycle was successfully created.' }
         format.json { render :show, status: :created, location: @reviews_cycle }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +28,13 @@ class ReviewsCyclesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews_cycles/1 or /reviews_cycles/1.json
   def update
     respond_to do |format|
       if @reviews_cycle.update(reviews_cycle_params)
-        format.html { redirect_to reviews_cycle_url(@reviews_cycle), notice: "Reviews cycle was successfully updated." }
+
+        format.html do
+          redirect_to reviews_cycle_url(@reviews_cycle), notice: 'Reviews cycle was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @reviews_cycle }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +43,22 @@ class ReviewsCyclesController < ApplicationController
     end
   end
 
-  # DELETE /reviews_cycles/1 or /reviews_cycles/1.json
   def destroy
     @reviews_cycle.destroy
 
     respond_to do |format|
-      format.html { redirect_to reviews_cycles_url, notice: "Reviews cycle was successfully destroyed." }
+      format.html { redirect_to reviews_cycles_url, notice: 'Reviews cycle was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reviews_cycle
-      @reviews_cycle = ReviewsCycle.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def reviews_cycle_params
-      params.require(:reviews_cycle).permit(:name, :organisation_id)
-    end
+  def set_reviews_cycle
+    @reviews_cycle = ReviewsCycle.find(params[:id])
+  end
+
+  def reviews_cycle_params
+    params.require(:reviews_cycle).permit(:name, :organisation_id)
+  end
 end
