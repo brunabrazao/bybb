efe94483d0ff3b881d9e854afd9510af539b8ad6
Bruna Brazao <brunaa@gmail.com>
2022-03-28 15:36:47 +0100

    Validate presence of report fields

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :user
-
+  validates :biggest_challenge, presence: true
+  validates :weekly_priority, presence: true
 
   def submitted_this_week?
     created_at.to_date.cweek == Date.current.cweek
