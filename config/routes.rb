cfed35e3928e29b62c7fb5525c4fea80a43c1eb8
Bruna Brazao <brunaa@gmail.com>
2022-03-29 22:47:58 +0100

    Update Posts controller
    I don't want to allow users to visit /posts as it's not really necessary.
    
    I still need to add the ability to delete a post after posting it to the
    feed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :reviews_cycles
   resources :feeds
-  resources :posts
+  resources :posts, only: %i[new create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
