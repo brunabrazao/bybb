6ec3f0d40851e04644b48cf112e0c611a9487620
Bruna Brazao <brunaa@gmail.com>
2022-04-01 17:33:35 +0100

    testing if devise invitable works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,17 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.default_url_options = { host: 'https://bybb-test.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '25',
+    domain: 'heroku.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
