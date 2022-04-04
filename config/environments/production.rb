188f0a158ac06f1e5226d8e899c2a82c86e589a2
Bruna Brazao <brunaa@gmail.com>
2022-04-04 18:20:26 +0100

    Change API key to password
    Now that I've finally identified it is a rails 7 thing (heroku does not
    recognise ActionMailer::Base.smtp_settings), I'm updating the password
    env to actually look for the password and not the api key.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: 'bybb-test.herokuapp.com' }
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
-    password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+    password: ENV['SENDGRID_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
     domain: 'bybb-test.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
